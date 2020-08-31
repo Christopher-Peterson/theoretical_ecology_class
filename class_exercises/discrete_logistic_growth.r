252f6bd7566838d6784214516dc03f49e1d130e8
Christopher-Peterson <christopher.r.peterson@gmail.com>
2020-08-31 13:14:01 -0500

    Simplified interface

--- class_exercises/discrete_logistic_growth.r
+++ class_exercises/discrete_logistic_growth.r
@@ -16,41 +16,38 @@ exponential_step = function(N, lambda = 1.2, ...) {
 # Run one of these step functions for a recursive sequence; 
 # define parameters with ...
 recursive_sequence = function(.func, gens = 75, N_0 = 2, ...) {
-
+  
+  init = tibble::tibble(N = N_0, t = 0, ...)
   # Wrap .func in a helper function that works with purrr::accumulate
-  helper_func = function(N_prev, t) {
-    .func(N_prev,...)
+  helper_func = function(prev, time) {
+    prev$t = time
+    prev$N = do.call(.func, prev)
+    prev
   }
   
   times = seq_len(gens)
   # accumulate() takes iterates over a function, taking the output 
     # as the first argument of the next iteration
-  Ns = accumulate(times, # iterate over time
+  out = accumulate(times, # iterate over time
                   .f = helper_func, # the function
-                  .init = N_0) # Starting value
-  # Put output in a data frame
-  tibble(N = Ns, 
-         t = c(0, times), # times doesn't include a spot for the initial value of 0
-         ...)
+                  .init = init) # Starting value
+  # out is a list of data frames
+  dplyr::bind_rows(out)
 }
 
 lambda_range = seq(.3, 2, by = .1)
 
 # run logistic sequence for each value of lambda_range, 
 # return results as data frame
-exp_lambda_results = map_dfr(lambda_range, 
-        ~recursive_sequence(.func = exponential_step, lambda = .x, , gens = 25))
-
-
-# range of r values to test
+exp_lambda_results = recursive_sequence(.func = exponential_step,
+                                        lambda = lambda_range , gens = 25)
+# Create logistic sequence function
+logistic_seq = partial(recursive_sequence, .func = logistic_step)
+# range of r & K values to test
 r_range = seq(-1, 2, by = .1)
-logistic_r_results = map_dfr(r_range,
-                             ~recursive_sequence(.func = logistic_step,
-                                                 r = .x))
 K_range = 10^seq(0.5, 3.5, by = .25)
-logistic_k_results = map_dfr(K_range,
-                             ~recursive_sequence(.func = logistic_step,
-                                                 K = .x))
+logistic_r_results = logistic_seq(r = r_range)
+logistic_K_results = logistic_seq(K = K_range)
 
 # Plots
 lambda_plot = ggplot(exp_lambda_results) + 
@@ -62,7 +59,7 @@ lambda_plot = ggplot(exp_lambda_results) +
   scale_color_viridis_c() + 
   scale_y_log10()
 
-K_plot = ggplot(logistic_k_results) + 
+K_plot = ggplot(logistic_K_results) + 
   aes(x = t, y = N, group = K, color = K) + 
   geom_line() + 
   scale_color_viridis_c(trans = "log10") + 
